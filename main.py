79149eb8b7ac556e660d3bfbea51ace2ecdb7ac0
Gandharv Dalal <gandharvdalal123@gmail.com>
2023-06-03 00:22:41 +0530

    done second exercise as well

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import openpyxl
 workbook=openpyxl.load_workbook(filename='inventory.xlsx')
 worksheet=workbook['Sheet1']
 products_per_supplier={}
+total_value_per_supplier={}
 
 for i in range(2,worksheet.max_row+1):
     suppliername=worksheet.cell(i,4).value
@@ -12,4 +13,15 @@ for i in range(2,worksheet.max_row+1):
         products_per_supplier[suppliername]=1
 
 
+    # for finding products_per_supplier
+    price=worksheet.cell(i,3).value
+    inventory=worksheet.cell(i,2).value
+    if suppliername in total_value_per_supplier:
+        temp2=total_value_per_supplier[suppliername]
+        total_value_per_supplier[suppliername]=temp2+inventory*price
+    else:
+        total_value_per_supplier[suppliername]=inventory*price    
+
+
 print(products_per_supplier)
+print(total_value_per_supplier)
